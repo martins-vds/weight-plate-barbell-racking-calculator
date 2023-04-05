849b2765a56d7757de7c6c2b8375f7afaf74137d
Vinny Martins <martins.vds@outlook.com>
2023-04-05 12:19:38 -0600

    update

--- src/WeightPlateBarbellRackingCalculator.Models/CalculationSettings.cs
+++ src/WeightPlateBarbellRackingCalculator.Models/CalculationSettings.cs
@@ -9,15 +9,19 @@ namespace WeightPlateBarbellRackingCalculator.Models
         [Range(1, int.MaxValue)]
         [Display(Name = "Total Weight")]
         public double TotalWeight { get; set; }
+
         [Required]
         [Range(0, int.MaxValue)]
         [Display(Name = "Bar Weight")]
         public double BarWeight { get; set; }
+        
         public IEnumerable<Plate>? Plates { get; set; }
+        
         [Required]
-        [Range(1, 100)]
+        [Range(1, int.MaxValue)]
         [Display(Name = "Weight Percentage")]
         public double WeightPercentage { get; set; } = 100;
+        
         [Range(1, 100)]
         [Display(Name = "Rounding")]
         public double Rounding { get; set; } = PlatesCalculatorConstants.NO_ROUNDING;
